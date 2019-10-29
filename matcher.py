b81c4b4470ff2cf138f94db1a4e73de910edfa33
Denis M <Nolane16@gmail.com>
2019-10-29 13:23:30 +0300

    Add hair

--- matcher.py
+++ matcher.py
@@ -37,6 +37,8 @@ def load_database(db_path):
 
 
 GOOD_FACE_SHAPES = [1, 2, 3, 5, 6]
+MALE_HAIRS = [9, 10, 11, 13, 17, 23, 31, 32, 37, 39, 43, 44, 45, 46, 49, 51, 52]
+FEMALE_HAIRS = [3, 4, 6, 9, 10, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 40, 42, 47, 48]
 
 
 def find_best_match(database, desired_parameters, is_male=None):
@@ -46,6 +48,7 @@ def find_best_match(database, desired_parameters, is_male=None):
     desired_parameters['face_shape'] = random.choice(GOOD_FACE_SHAPES)
     desired_parameters['glasses'] = 11
     desired_parameters['facial_hair'] = 14
+    desired_parameters['hair'] = random.choice(MALE_HAIRS if is_male else FEMALE_HAIRS)
     images = database.items()
     for parameter in PARAMETERS:
         if parameter in desired_parameters:
