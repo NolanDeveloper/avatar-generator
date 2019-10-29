950847d6b500f8838dafe9c8e373bb0f2809555f
Oleg Stotsky <ostotsky@gmail.com>
2019-10-29 12:33:26 +0300

    add skin color integration into pipeline

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from matcher import find_best_match, load_database
 import os
 
 from get_eye_color import get_eye_color
+from face_color_classifier import get_skin_color_number
 
 app = Flask(__name__)
 CORS(app)
@@ -24,8 +25,9 @@ def generate_avatar():
     file_name = convert_base64_pic_to_ndarray(re.sub(r"data:image\/\w+;base64,", "", content['pic']))
     image = cv2.imread(file_name)
     eye_color = get_eye_color(image)
+    skin_color = get_skin_color_number(image)
     cartoon_database = load_database(DB_PATH)
-    params = { eye_color: eye_color }
+    params = { 'eye_color': eye_color, 'face_color': skin_color }
     pic_for_user = find_best_match(cartoon_database, params)[:-3] + 'png'
     with open(pic_for_user, 'rb') as f:
         encoded_file = base64.b64encode(f.read())
