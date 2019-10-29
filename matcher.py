d6ae910698d5cdabc801939fa211161f976d6204
Denis M <Nolane16@gmail.com>
2019-10-29 14:39:38 +0300

    Fix gender

--- matcher.py
+++ matcher.py
@@ -37,14 +37,14 @@ def load_database(db_path):
 
 
 GOOD_FACE_SHAPES = [1, 2, 3, 5, 6]
-MALE_HAIRS = [9, 10, 11, 13, 17, 23, 31, 32, 37, 39, 43, 44, 45, 46, 49, 51, 52]
+MALE_HAIRS = [10, 11, 17, 23, 31, 32, 37, 39, 43, 44, 45, 46, 49, 51, 52]
 FEMALE_HAIRS = [3, 4, 6, 9, 10, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 40, 42, 47, 48]
 
 
 def find_best_match(database, desired_parameters, is_male=None):
     if is_male is None:
         is_male = bool(random.getrandbits(1))
-    desired_parameters['eye_lashes'] = int(not is_male)
+    desired_parameters['eye_lashes'] = int(is_male)
     desired_parameters['face_shape'] = random.choice(GOOD_FACE_SHAPES)
     desired_parameters['glasses'] = 11
     desired_parameters['facial_hair'] = 14
@@ -52,10 +52,10 @@ def find_best_match(database, desired_parameters, is_male=None):
     images = database.items()
     for parameter in PARAMETERS:
         if parameter in desired_parameters:
-            new_mages = list(filter(lambda image: image[1][parameter] == desired_parameters[parameter], images))
-            if not new_mages:
+            new_images = list(filter(lambda image: image[1][parameter] == desired_parameters[parameter], images))
+            if not new_images:
                 break
-            images = new_mages
+            images = new_images
     return random.choice(images)[0]
 
 
